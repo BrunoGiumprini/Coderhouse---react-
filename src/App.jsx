9b36f03b364c5e64b76d65be8e09efa9ed4abd77
BrunoGiumprini <106162844+BrunoGiumprini@users.noreply.github.com>
2025-12-18 07:24:13 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />
-    </>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/item/:id" element={<ItemDetailContainer />} />
+        <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
+      </Routes>
+    </Router>
   );
 }
 
